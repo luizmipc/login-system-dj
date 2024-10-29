1afd4c80830ef4e8761c3adfb11dbfe6619f7b98
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-10-29 15:08:00 -0300

    chore: update gitignore

--- login-system-dj/config/settings.py
+++ login-system-dj/config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
 
     # dependencies
     'widget_tweaks',
