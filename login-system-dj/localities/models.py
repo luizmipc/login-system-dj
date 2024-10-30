0ca33a86013eea897ca89c720e863abdbe911bc3
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-10-30 17:06:57 -0300

    feat: Created Country, State, and Address models

--- login-system-dj/localities/models.py
+++ login-system-dj/localities/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class State(models.Model):
+    country = models.ForeignKey(Country, related_name='states', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return f"{self.name}, {self.country.code}"
+
+class Address(models.Model):
+    state = models.ForeignKey(State, related_name='addresses', on_delete=models.CASCADE)
+    city = models.CharField(max_length=100)
+    neighborhood = models.CharField(max_length=100, blank=True, null=True)
+    street_name = models.CharField(max_length=150)
+    street_number = models.CharField(max_length=10)
+    postal_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street_name}, {self.street_number}, {self.city}, {self.state.name}, {self.state.country.name}"
