f6f31c20a56f4ad6d02edd098b7ea70318a252ac
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-11-02 13:47:21 -0300

    feat: Added Locality field

--- login-system-dj/accounts/models.py
+++ login-system-dj/accounts/models.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-  pass
+    pass
+    locality = models.ForeignKey(
+        "localities.Locality",  # Referencing the 'Locality' model in the 'localities' app
+        related_name='users',  # Gives a reverse access from Locality to CustomUser
+        verbose_name="Locality",
+        on_delete=models.CASCADE
+    )
 
-  def __str__(self):
-    return self.username
+    def __str__(self):
+        return self.username
