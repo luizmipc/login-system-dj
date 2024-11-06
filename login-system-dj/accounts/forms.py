d26c6244c335dd5bda66f50ccc61fe7bf60e5ae1
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-11-05 22:47:00 -0300

    feat: Added ids for future tests

--- login-system-dj/accounts/forms.py
+++ login-system-dj/accounts/forms.py
@@ -1,11 +1,43 @@
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
-
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from accounts.models import CustomUser
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
 from django import forms
 
 """ Maintain users """
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput
+    )
 
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "date_of_birth"]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model =CustomUser
+        fields = ["username", "email", "password", "date_of_birth", "is_active", "is_admin"]
 
 
