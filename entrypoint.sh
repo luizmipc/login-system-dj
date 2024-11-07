cf9f460a90f3d1f16f5c83fc66b1c1cbceb18d76
Luiz Marcelo <99672783+luizmipc@users.noreply.github.com>
2024-11-07 20:05:05 -0300

    test cypress workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Run migrations
-python manage.py makemigrations
-python manage.py migrate --noinput
+python manage.py makemigrations accounts
+python manage.py migrate
 
 # Start the Django development server
 exec python manage.py runserver 0.0.0.0:8000
